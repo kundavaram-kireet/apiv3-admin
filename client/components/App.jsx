34eaec2f144f46cc963c4493cf3017e8be949076
Kireet R. Kundavaram <kundavaram.kireet@dol.gov>
2017-07-17 11:47:59 -0400

    Navbar version 2

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,17 +5,39 @@ import Navbar from 'react-navbar';
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
+    <div style={{textAlign: 'center'}}>
         <h1>Admin UI</h1>
-        	<h2 style={{textAlign: 'left'}}> navi bar </h2>
+        	<h2 style={{textAlign: 'left'}}> Navigation Bar </h2>
       <nav>
-        <ul>
-          <li style={{textAlign: 'left'}}><a href="#">Users</a></li>
-          <li style={{textAlign: 'left'}}><a href="#">API Keys</a></li>
-          <li style={{textAlign: 'left'}}><a href="#">Dataset</a></li>
-        </ul>
+      	<div className="dropdown" style={{textAlign: 'left'}}>
+      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Users
+			<span className="caret"></span></button>
+        		<ul className="dropdown-menu">
+        			<li className="divider"><a href="#">User 1</a></li>
+   	   				<li className="divider"><a href="#">User 2</a></li>
+    				<li className="divider"><a href="#">User 3</a></li>
+ 				 </ul>
+        </div>
+      	<div className="dropdown" style={{textAlign: 'left'}}>
+      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dataset
+			<span className="caret"></span></button>
+        		<ul className="dropdown-menu">
+        			<li className="divider"><a href="#">Dataset 1</a></li>
+   	   				<li className="divider"><a href="#">Dataset 2</a></li>
+    				<li className="divider"><a href="#">Dataset 3</a></li>
+ 				 </ul>
+        </div>
+      	<div className="dropdown" style={{textAlign: 'left'}}>
+      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">API Keys
+			<span className="caret"></span></button>
+        		<ul className="dropdown-menu">
+        			<li className="divider"><a href="#">API Key 1</a></li>
+   	   				<li className="divider"><a href="#">API Key 2</a></li>
+    				<li className="divider"><a href="#">API Key 3</a></li>
+ 				 </ul>
+        </div>
       </nav>        	
-      </div>);
+    </div>);
  
 }
 }
