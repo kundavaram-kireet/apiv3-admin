7d05ee33d98abbf0b923d238bc0ff93bb95b92ff
Kireet R. Kundavaram <kundavaram.kireet@dol.gov>
2017-07-18 15:59:15 -0400

    Version 3 updates

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,35 +8,41 @@ export default class App extends React.Component {
     <div style={{textAlign: 'center'}}>
         <h1>Admin UI</h1>
         	<h2 style={{textAlign: 'left'}}> Navigation Bar </h2>
-      <nav>
-      	<div className="dropdown" style={{textAlign: 'left'}}>
-      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Users
-			<span className="caret"></span></button>
-        		<ul className="dropdown-menu">
-        			<li className="divider"><a href="#">User 1</a></li>
-   	   				<li className="divider"><a href="#">User 2</a></li>
-    				<li className="divider"><a href="#">User 3</a></li>
- 				 </ul>
-        </div>
-      	<div className="dropdown" style={{textAlign: 'left'}}>
-      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dataset
-			<span className="caret"></span></button>
-        		<ul className="dropdown-menu">
-        			<li className="divider"><a href="#">Dataset 1</a></li>
-   	   				<li className="divider"><a href="#">Dataset 2</a></li>
-    				<li className="divider"><a href="#">Dataset 3</a></li>
- 				 </ul>
-        </div>
-      	<div className="dropdown" style={{textAlign: 'left'}}>
-      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">API Keys
-			<span className="caret"></span></button>
-        		<ul className="dropdown-menu">
-        			<li className="divider"><a href="#">API Key 1</a></li>
-   	   				<li className="divider"><a href="#">API Key 2</a></li>
-    				<li className="divider"><a href="#">API Key 3</a></li>
- 				 </ul>
-        </div>
-      </nav>        	
+	    <nav>
+	      	<div className="dropdown" style={{textAlign: 'left'}}>
+	      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Users
+				<span className="caret"></span></button>
+	        		<ul className="dropdown-menu">
+	        			<li><a href="#">User 1</a></li>
+	        			<li className="divider"></li>
+	   	   				<li><a href="#">User 2</a></li>
+	   	   				<li className="divider"></li>
+	    				<li><a href="#">User 3</a></li>
+	 				 </ul>
+	        </div>
+	      	<div className="dropdown" style={{textAlign: 'left'}}>
+	      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Datasets
+				<span className="caret"></span></button>
+	        		<ul className="dropdown-menu">
+	        			<li><a href="#">Dataset 1</a></li>
+	        			<li className="divider"></li>
+	   	   				<li><a href="#">Dataset 2</a></li>
+	   	   				<li className="divider"></li>
+	    				<li><a href="#">Dataset 3</a></li>
+	 				 </ul>
+	        </div>
+	      	<div className="dropdown" style={{textAlign: 'left'}}>
+	      		<button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">API Keys
+				<span className="caret"></span></button>
+	        		<ul className="dropdown-menu">
+	        			<li><a href="#">API Key 1</a></li>
+	        			<li className="divider"></li>
+	   	   				<li><a href="#">API Key 2</a></li>
+	   	   				<li className="divider"></li>
+	    				<li><a href="#">API Key 3</a></li>
+	 				 </ul>
+	        </div>
+        </nav>        	
     </div>);
  
 }
